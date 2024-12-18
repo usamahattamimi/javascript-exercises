3c77784821931d609472ef4958e454a846be1dd9
usamahattamimi <usamahattamimi.1996@gmail.com>
2024-12-18 22:31:07 +0700

    Subject: Complete Exercise 2 repeatString Description: edit file repeatString.js to make the function running as expected and pass the tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+const repeatString = function (text, repeat) {
+  let result = "";
+  let count = 0;
 
+  if (repeat >= 0) {
+    // for loop
+    // for (let i = 0; i < repeat; i++) {
+    //   result += text;
+    // }
+
+    // while loop
+    while (count < repeat) {
+      result += text;
+      count++;
+    }
+  } else result = "ERROR";
+
+  return result;
 };
 
 // Do not edit below this line
