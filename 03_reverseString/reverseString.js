8338a9152d1079ab876f4d91a3b708ab1c4be80f
usamahattamimi <usamahattamimi.1996@gmail.com>
2024-12-19 20:30:41 +0700

    Subject: finish exercise 3 reverse string Description: reverse function parameter from argument

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (word) {
+  //   let result = [];
+  //   if (word)
+  //     for (let i = word.length; i > 0; i--) {
+  //       result.push(word[i - 1]);
+  //     }
+  //   else return (result = "");
+  //   return result.join("");
 
+  return word.split("").reverse().join("");
 };
 
 // Do not edit below this line
