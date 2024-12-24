ea12e76711ccd5a0542db56d20f51b0b472a4171
usamahattamimi <usamahattamimi.1996@gmail.com>
2024-12-24 14:53:18 +0700

    Subject: finish exercise 04 remove from array Description: make the function removeFromArray return only if the param1 is not the same from param2, and other

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  // method 1
+  // // const resultArrays = arguments[0];
+  // // const filter = Array.from(arguments).slice(1);
+
+  // return array.filter((result) => !args.includes(result));
+
+  // method 2
+  const result = [];
+
+  array.forEach((item) => {
+    if (!args.includes(item)) result.push(item);
+  });
+
+  return result;
 };
 
 // Do not edit below this line
