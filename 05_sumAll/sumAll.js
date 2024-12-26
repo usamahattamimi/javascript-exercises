523020cbc1567c25c64d6e5500c84dd3ee8eceb2
usamahattamimi <usamahattamimi.1996@gmail.com>
2024-12-26 10:42:31 +0700

    Subject: finish exercise 05 sumAll parameters Description: make the sumAll function work as expected

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  let result = 0;
 
+  if (
+    start < 0 ||
+    end < 0 ||
+    !Number.isInteger(start) ||
+    !Number.isInteger(end)
+  )
+    return "ERROR";
+
+  if (start > end) [start, end] = [end, start];
+
+  for (let i = start; i <= end; i++) {
+    result += i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
