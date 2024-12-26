7396274c492bdf8feb37c76970d3dbeee174eaf4
usamahattamimi <usamahattamimi.1996@gmail.com>
2024-12-26 14:09:53 +0700

    finish exercise 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,54 @@
-const add = function() {
-	
+const add = function (first, second) {
+  return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function (first, second) {
+  return first - second;
 };
 
-const sum = function() {
-	
+const sum = function (number) {
+  let result = 0;
+  let count = 0;
+
+  while (count < number.length) {
+    result += number[count];
+    count++;
+  }
+
+  return result;
 };
 
-const multiply = function() {
+const multiply = function (numbers) {
+  let result = 1;
 
+  for (const number of numbers) {
+    result *= number;
+  }
+
+  return result;
 };
 
-const power = function() {
-	
+const power = function (first, second) {
+  let result = first;
+  let count = 0;
+
+  while (count < second - 1) {
+    result *= first;
+    count++;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number === 0) return 1;
+
+  let result = 1;
+  for (let i = 1; i <= number; i++) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +58,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
