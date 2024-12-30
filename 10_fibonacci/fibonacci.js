a79fb2de2160df8f90db95a4314bbc92131ecf19
usamahattamimi <usamahattamimi.1996@gmail.com>
2024-12-30 15:24:32 +0700

    finish exercise 10 fibonacci in javascript

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,36 @@
-const fibonacci = function() {
+// const fibonacci = function (n) {
+//   if (n < 0) return "OOPS";
 
+//   if (typeof n === "string") n = Number(n);
+
+//   const fibonacci = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+//   }
+
+//   return fibonacci[n];
+// };
+
+const fibonacci = (n) => {
+  let count;
+
+  if (typeof n === "string") count = parseInt(n);
+  else count = n;
+
+  if (count < 0) return "OOPS";
+  if (count === 0) return 0;
+
+  let firstPrevious = 1;
+  let secondPrevious = 0;
+
+  for (let i = 2; i <= count; i++) {
+    let current = firstPrevious + secondPrevious;
+    secondPrevious = firstPrevious;
+    firstPrevious = current;
+  }
+
+  return firstPrevious;
 };
 
 // Do not edit below this line
