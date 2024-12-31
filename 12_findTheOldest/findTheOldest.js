c0bbb8f774dd360f7caf7cfadba9ab66c29a7b5c
usamahattamimi <usamahattamimi.1996@gmail.com>
2024-12-31 14:19:16 +0700

    Subject: finish last exercise get the oldest person in javascript exercises Description: make function to return the oldest person from array of objects parameter

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,35 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  //   const currentYear = new Date().getFullYear();
 
+  //   const personWithAge = people.reduce((acc, curr) => {
+  //     const yearOfDeath = curr.yearOfDeath || currentYear;
+
+  //     acc.push({
+  //       ...curr,
+  //       age: yearOfDeath - curr.yearOfBirth,
+  //     });
+
+  //     return acc;
+  //   }, []);
+
+  //   const oldestPerson = personWithAge.sort((a, b) => b.age - a.age)[0];
+
+  //   return oldestPerson;
+
+  const currentYear = new Date().getFullYear();
+
+  const oldestPerson = people.reduce((acc, curr) => {
+    const yearOfDeath = curr.yearOfDeath || currentYear;
+    const age = yearOfDeath - curr.yearOfBirth;
+
+    if (!acc || age > acc.age) {
+      return { ...curr, age };
+    }
+
+    return acc;
+  }, null);
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
